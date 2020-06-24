acfddd33688cb7275b7cad6badfaf36cc0f26336
Navkar Shah <shahnavkar15@gmail.com>
2020-06-24 14:43:41 +0530

    .

--- src/c_src/bootstrap/Sliding View/style.js
+++
@@ -1,5 +0,0 @@
-
-import React, {Component} from 'react';
-import {StyleSheet} from 'react-native';
-
-export default StyleSheet.create({});
--- src/components/library/YourScreen/uploads/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {View} from 'react-native'
-
-export default function UploadsList({navigation}){
-    return(
-        <View style={{flex:1, backgroundColor:'grey'}}/>
-    )
-}
